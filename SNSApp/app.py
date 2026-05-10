2cc4183d21fa9f71b0312108c38282fab900034a
tmtk.43z <tomocky721@icloud.com>
2026-05-10 22:24:24 +0900

    開発環境整備および今までの成果物反映。

--- SNSApp/app.py
+++ SNSApp/app.py
@@ -111,10 +111,15 @@ def threads_view():
     else:
         threads = Thread.get_all()
         for thread in threads:
-            thread["created_at"] = thread["created_at"].strftime("%Y-%m-%d %H:%M")
-            thread["user_name"] = User.get_name_by_id(thread)
-            posts = Post.get_few()
-        return render_template("/threads.html")
+            thread["created_at"] = thread["created_at"].strftime("%Y/%m/%d %H:%M")
+            thread["user_name"] = User.get_name_by_id(thread["user_id"])
+            #最新3件のポストを表示
+            posts = Post.get_few(thread["id"])
+            #リアクション数を表示
+            reaction_counts = Reaction.count(thread["id"])
+            #コメント数を表示
+            comment_counts = Comment.count(thread["id"])
+        return render_template("/thread/thread_time_line.html", threads = threads, posts = posts, reaction_counts = reaction_counts, comment_counts = comment_counts)
     
 #スレッド作成画面の表示
 @app.route("/threads/new", methods=["GET"])
@@ -123,7 +128,7 @@ def new_thread_view():
     if user_id is None:
         return redirect(url_for("login_view"))
     else:
-        return render_template("/threads/new.html")
+        return render_template("/thread/new.html")
 
 #スレッド作成処理
 @app.route("/threads", methods=["POST"])
@@ -131,15 +136,207 @@ def create_thread():
     user_id = session.get("user_id")
     if user_id is None:
         return redirect(url_for("login_view"))
-    title = request.form.get("title", "").strip()
-    image = request.form.get("", "")
-    theme_id = request.form.get("theme", "")
-    if title == "":
-        flash("タイトルが空です", "error")
-    elif theme_id == "":
-        flash("趣旨を選んでください", "error")
     else:
-        thread_id = uuid.uuid4().bytes
-        Thread.create(thread_id, user_id, title, image, theme_id)
-        flash("スレッドを作成しました", "success")
-        return redirect(url_for("user_threads_view"))
+        title = request.form.get("title", "").strip()
+        image = request.form.get("", "")
+        theme_id = request.form.get("theme", "")
+        if title == "":
+            flash("タイトルが空です", "error")
+        elif theme_id == "":
+            flash("趣旨を選んでください", "error")
+        else:
+            thread_id = uuid.uuid4().bytes
+            Thread.create(thread_id, user_id, title, image, theme_id)
+            flash("スレッドを作成しました", "success")
+            return redirect(url_for("user_threads_view"))
+
+#スレッド詳細画面の表示
+@app.route("/threads/<uuid:thread_id>", methods=["GET"])
+def thread_detail_view(thread_id):
+    user_id = session.get("user_id")
+    if user_id is None:
+        return redirect(url_for("login_view"))
+    else:
+        #スレッドを表示
+        thread = Thread.find_by_id(thread_id)
+        if thread is None:
+            abort(404)
+        thread["created_at"] = thread["created_at"].strftime("%Y/%m/%d %H:%M")
+        #リアクション数を表示
+        reaction_counts = Reaction.count(thread_id)
+        #コメント数を表示
+        comment_counts = Comment.count(thread_id)
+        #ポストを表示
+        posts = Post.get_all(thread_id)
+        for post in posts:
+            post["created_at"] = post["created_at"].strftime("%Y/%m/%d %H:%M")
+        return render_template("thread/thread_detail.html", thread = thread, reaction_counts = reaction_counts, comment_counts = comment_counts, posts = posts)
+
+#スレッド削除処理
+@app.route("/threads/<uuid:thread_id>/delete", methods=["POST"])
+def delete_thread(thread_id):
+    user_id = session.get("user_id")
+    if user_id is None:
+        return redirect(url_for("login_view"))
+    else:
+        thread = Thread.find_by_id(thread_id)
+        if thread is None:
+            abort(404)
+        #自分のスレッドのみ削除可能
+        elif thread["user_id"] != user_id:
+            flash("このスレッドを削除することはできません", "error")
+            return redirect(url_for("thread_detail_view"))
+        else:
+            Thread.delete(thread_id)
+            flash("スレッドを削除しました", "success")
+            return redirect(url_for("user_threads_view"))
+
+
+#コメント一覧ページ表示
+@app.route("/threads/<uuid:thread_id>/comments", methods=["GET"])
+def comments_view(thread_id):
+    user_id = session.get("user_id")
+    if user_id is None:
+        return redirect(url_for("login_view"))
+    else:
+        #スレッドを表示
+        thread = Thread.find_by_id(thread_id)
+        if thread is None:
+            abort(404)
+        thread["created_at"] = thread["created_at"].strftime("%Y/%m/%d %H:%M")
+        #リアクション数を表示
+        reacition_counts = Reaction.count(thread_id)
+        #コメント数を表示
+        comments_counts = Comment.count(thread_id)
+        #コメントを表示
+        comments = Comment.get_all(thread_id)
+        for comment in comments:
+            comment["created_at"] = comment["created_at"].strftime("%Y/%m/%d %H:%M")
+            comment["user_name"] = User.get_name_by_id(comment["user_id"])
+        return render_template("/", thread = thread, reacition_counts = reacition_counts, comments_counts = comments_counts, comments = comments)
+
+#コメント投稿画面の表示
+@app.route("/threads/<uuid:thread_id>/comments/new", methods=["GET"])
+def new_comment_view(thread_id):
+    user_id = session.get("user_id")
+    if user_id is None:
+        return redirect(url_for("login_view"))
+    else:
+        thread = Thread.find_by_id(thread_id)
+        if thread is None:
+            abort (404)
+        else:
+            return render_template("/")
+
+#コメント投稿処理
+@app.route("/threads/<uuid:thread_id>/comments/new", methods=["POST"])
+def create_comment(thread_id):
+    user_id = session.get("user_id")
+    if user_id is None:
+        return redirect(url_for("login_view"))
+    else:
+        content = request.form.get("content", "").strip()
+        if content == "":
+            flash("コメントが空です", "error")
+        else:
+            comment_id = uuid.uuid4().bytes
+            Comment.create(comment_id, user_id, thread_id, content)
+            flash("コメントを投稿しました", "error")
+            return redirect(url_for("comments_view", thread_id = thread_id))
+
+#コメント削除処理
+@app.route("/threads/<uuid:thread_id>/comments/<uuid:comment_id/delete", methods=["POST"])
+def delete_comment(thread_id, comment_id):
+    user_id = session.get("user_id")
+    if user_id is None:
+        return redirect(url_for("login_view"))
+    else:
+        comment = Comment.find_by_id(comment_id)
+        if comment is None:
+            abort(404)
+        elif comment["user_id"] != user_id:
+            flash("このコメントを削除することはできません", "error")
+            return redirect(url_for("comments_view", thread_id = thread_id))
+        else:
+            Comment.delete(comment_id)
+            flash("コメントを削除しました", "success")
+            return redirect(url_for("comments_view", thread_id = thread_id))
+
+#ポスト作成処理
+@app.route("/threads/<uuid:thread_id>/posts", methods = ["POST"]) 
+def create_post(thread_id):
+    user_id = session.get("user_id")
+    if user_id is None:
+        return redirect(url_for('login_view'))
+    content = request.form.get("post", "").strip()
+    image = request.form.get("") #imageをどう書くか？
+    count = request.form.get("count", "").strip()
+    rep = request.form.get("rep", "").strip()
+  
+
+    if content == "":
+        flash("トレーニング記録を入力してください", "error")
+        return redirect(url_for("thread_detail_view"))
+    elif count == "":
+        flash("トレーニング回数を入力してください", "error")
+        return redirect(url_for('', thread_id = thread_id)) 
+    else:
+        post_id = uuid.uuid4().bytes
+        Post.create(post_id, user_id, thread_id, content, image, count, rep)
+        flash("トレーニング記録を作成しました", "success")
+        return redirect(url_for("thread_detail_view", thread_id = thread_id))
+    
+#ポスト削除処理
+@app.route("/threads/<uuid:thread_id>/posts/<uuid:post_id>/delete", methods = ["POST"])
+def post_delete(post_id, thread_id):
+    user_id = session.get("user_id")
+    if user_id is None:
+        return redirect(url_for('login_view'))
+    
+    post = Post.find_by_id(post_id)
+
+    if post is None:
+        flash("トレーニング記録がありません", "error")
+        return redirect(url_for("thread_detail_view", thread_id = thread_id))
+    
+    if post['user_id'] != user_id:
+        flash("このトレーニング記録は削除できません", "error")
+        return redirect(url_for("thread_detail_view", thread_id = thread_id))
+    
+    Post.delete(post_id)
+    flash("トレーニング記録が削除されました", "success")
+    return redirect(url_for("thread_detail_view", thread_id = thread_id))
+
+#ポスト一覧表示機能⇒thread詳細表示機能にpost表示を含めている
+
+#リアクション送信機能
+@app.route("/threads/<uuid:thread_id>", methods = ["POST"])
+def create_reaction(thread_id):
+    user_id = session.get("user_id")
+    if user_id is None:
+        return redirect(url_for('login_view'))
+    
+    reaction_id = request.form.get("reaction") #HTML変数確認
+
+    if reaction_id == "":
+        flash("リアクション内容がありません", "error")
+        return redirect(url_for("thread_detail_view", thread_id = thread_id))
+    
+    #すでに送信済みのリアクションかを確認
+    existing_reaction = Reaction.find_same_reaction(user_id, thread_id, reaction_id)
+
+    if existing_reaction is None:
+        #初めてリアクションを送る場合
+        Reaction.create(user_id, thread_id, reaction_id)
+        flash('リアクションを送信しました', 'success')
+        return redirect(url_for("thread_detail_view", thread_id = thread_id))
+
+    #すでに同一リアクションを送っている場合はDBをUPDATEする
+    else:
+        Reaction.update(user_id, thread_id, reaction_id)
+        flash("リアクションを送信しました", "success")
+        return redirect(url_for("thread_detail_view", thread_id = thread_id))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
--- SNSApp/models.py
+++ SNSApp/models.py
@@ -15,7 +15,7 @@ class User:
         conn = db_pool.get_conn()
         try:
             with conn.cursor() as cur:
-                sql = "INSERT INTO Users (id, user_name, email_address, password) VALUES (%s, %s, %s, %s);"
+                sql = "INSERT INTO users (id, user_name, email_address, password) VALUES (%s, %s, %s, %s);"
                 cur.execute(sql, (user_id, user_name, email_address, password))
                 conn.commit()
                 return user_id
@@ -34,12 +34,28 @@ class User:
         conn = db_pool.get_conn()
         try:
             with conn.cursor() as cur:
-                sql = "SELECT * FROM Users WHERE email_address = %s;"
+                sql = "SELECT * FROM users WHERE email_address = %s;"
                 cur.execute(sql, (email_address,))
                 user = cur.fetchone() 
             return user
         except pymysql.Error as e:
             print(f'エラーが発生しています：{e}')
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+
+    @classmethod
+    def get_name_by_id(cls, user_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT user_name FROM users WHERE id=%s;"
+                cur.execute(sql, (user_id,))
+                user = cur.fetchone()
+            return user
+        except pymysql.Error as e:
+            print(f"エラーが発生しています:{e}")
             abort(500)
         finally:
             db_pool.release(conn)
@@ -51,7 +67,7 @@ class Thread:
         conn = db_pool.get_conn()
         try:
             with conn.cursor() as cur:
-                sql = "SELECT * FROM threads ORDER BY created_at DESC;"
+                sql = "SELECT * FROM threads WHERE deleted_at IS NULL ORDER BY created_at DESC;"
                 cur.execute(sql)
                 threads = cur.fetchall()
             return threads
@@ -69,6 +85,247 @@ class Thread:
                 sql = "INSERT INTO threads (id, user_id, title, image, thme_id) VALUE (%s, %s, %s, %s, %s);"
                 cur.execute(sql, (thread_id, user_id, title, image, theme_id))
                 conn.commit()
+        except pymysql.Error as e:
+            print(f"エラーが発生しています:{e}")
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+    @classmethod
+    def find_by_id(cls, thread_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT * FROM threads WHERE id=%s AND deleted_at IS NULL;"
+                cur.execute(sql, (thread_id,))
+                thread = cur.fetchone()
+            return thread
+        except pymysql.Error as e:
+            print(f"エラーが発生しています:{e}")
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+    @classmethod
+    def delete(cls, thread_id):
+        conn =db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "UPDATE threads SET deleted_at = NOW() WHERE id=%s;"
+                cur.execute(sql, (thread_id,))
+                cur.commit()
+        except pymysql.Error as e:
+            print(f"エラーが発生しています:{e}")
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+
+#Postクラス
+class Post:
+    @classmethod
+    def get_all(cls, thread_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT * FROM posts WHERE id=%s AND deleted_at IS NULL ORDER BY created_at DESC;"
+                cur.execute(sql, (thread_id,))
+                posts = cur.fetchall()
+            return posts
+        except pymysql.Error as e:
+            print(f"エラーが発生しています:{e}")
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+    @classmethod
+    def get_few(cls, thread_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT * FROM posts WHERE id=%s AND deleted_at IS NULL ORDER BY created_at DESC LIMIT 3;"
+                cur.execute(sql, (thread_id,))
+                posts = cur.fetchall()
+            return posts
+        except pymysql.Error as e:
+            print(f"エラーが発生しています:{e}")
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+    #ポストidでの検索
+    def find_by_id(cls, post_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT * FROM posts WHERE id = %s AND deleted_at IS NULL"
+                cur.execute(sql, (post_id,))
+                post = cur.fetchone()
+            return post
+        except pymysql.Error as e:
+            print(f"エラーが発生しています：{e}")
+        finally:
+            db_pool.release(conn)
+
+    #ポスト作成
+    @classmethod
+    def create(cls, post_id, user_id, thread_id, content, image, count, rep):
+        post_id = uuid.uuid4().bytes #uuid4でpost_idを生成してバイナリ形式に変換
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "INSERT INTO posts(id, user_id, thread_id, content, image, count, rep) VALUE (%S, %S, %S, %S, %S, %S, %S);"
+                cur.execute(sql,(post_id, user_id, thread_id, content, image, count, rep))
+                conn.commit()
+        except pymysql.Error as e:
+            print(f"エラーが発生しています：{e}")
+            abort(500)
+        finally:
+            db_pool.release(conn) 
+
+    
+    #ポスト削除機能
+    @classmethod
+    def delete(cls, post_id):
+        conn =db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "UPDATE posts SET deleted_at = NOW() WHERE id = %s;"
+                cur.execute(sql, (post_id,))
+                conn.commit()
+        except pymysql.Error as e:
+            print(f"エラーが発生しています：{e}")
+        finally:
+            db_pool.release(conn)
+
+#Reactionクラス
+class Reaction:
+    @classmethod
+    def find_same_reaction(cls, user_id, thread_id, reaction_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT * FROM thread_reactions WHERE user_id = %s AND thread_id = %s AND reaction_id = %s"
+                cur.execute(sql, (user_id, thread_id, reaction_id))
+                reaction = cur.fetchone()
+            return reaction
+        except pymysql.Error as e:
+            print(f'エラーが発生しています：{e}')
+        finally:
+            db_pool.release(conn)
+
+    @classmethod
+    def create(cls, user_id, thread_id, reaction_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "INSERT INTO thread_reactions(user_id, thread_id, reaction_id) VALUES(%s, %s, %s);" #reaction_countカラムへの入力は不要？
+                cur.execute(sql, (user_id, thread_id, reaction_id))
+                conn.commit()
+                #reaction_idはAuto_increment
+        except pymysql.Error as e:
+            print(f"エラーが発生しています：{e}")
+        finally:
+            db_pool.release(conn)
+
+    @classmethod
+    def update(cls, user_id, thread_id, reaction_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                #リアクション上限到達確認
+                sql = "SELECT reaction_count FROM thread_reactions WHERE user_id = %s AND thread_id = %s AND reaction_id = %s"
+                cur.execute(sql, (user_id, thread_id, reaction_id))
+                row = cur.fetchone()
+                if row is None:
+                    print(f"該当するリアクションがありません")
+                    return
+                
+                current_reaction_count = row [0]
+
+                if current_reaction_count >= 100:
+                    print("リアクション数が上限に達しています")
+                    return
+                #reaction_countに+1する
+                else:
+                    sql = "UPDATE thread_reactions SET reaction_count = reaction_count + 1 WHERE user_id = %s AND thread_id = %s AND reaction_id = %s" #default設定が必要？
+                    cur.execute(sql, (user_id, thread_id, reaction_id))             
+                    conn.commit()
+        except pymysql.Error as e:
+            print(f"エラーが発生しています：{e}")
+        finally:
+            db_pool.release(conn)
+        
+    @classmethod
+    def count(cls, thread_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT reaction_id, reaction_name, SUM(reaction_count) FROM thread_reactions INNER JOIN reactions ON thread_reactions.reaction_id = reactions.id WHERE thread_id=%s GROUP BY reaction_id;"
+                cur.execute(sql, (thread_id,))
+                reaction_counts = cur.fetchall()
+            return reaction_counts
+        except pymysql.Error as e:
+            print(f"エラーが発生しています:{e}")
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+
+#Commentクラス
+class Comment:
+    @classmethod
+    def get_all(cls, thread_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT * FROM comments WHERE thread_id=%s AND deleted_at IS NULL ORDER BY created_at DESC;"
+                cur.execute(sql, (thread_id,))
+                comments = cur.fetchall()
+            return comments
+        except pymysql.Error as e:
+            print(f"エラーが発生しています:{e}")
+            abort(500)
+        finally:
+            db_pool.release(conn)
+    
+    @classmethod
+    def count(cls, thread_id):
+        conn =db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "SELECT COUNT(id) FROM comments WHERE thread_id=%s AND deleted_at IS NULL;"
+                cur.execute(sql, (thread_id,))
+                comment_counts = cur.fetchone()
+            return comment_counts
+        except pymysql.Error as e:
+            print(f"エラーが発生しています:{e}")
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+    @classmethod
+    def create(cls, comment_id, user_id, thread_id, content):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "INSERT INTO comments (id, user_id, thread_id, content) VALUE (%s, %s, %s, %s);"
+                cur.execute(sql, (comment_id, user_id, thread_id, content))
+                cur.commit()
+        except pymysql.Error as e:
+            print(f"エラーが発生しています:{e}")
+            abort(500)
+        finally:
+            db_pool.release(conn)
+
+    @classmethod
+    def delete(cls, comment_id):
+        conn = db_pool.get_conn()
+        try:
+            with conn.cursor() as cur:
+                sql = "UPDATE comments SET deleted_at = NOW() WHERE id=%s;"
+                cur.execute(sql, (comment_id,))
+                cur.commit()
         except pymysql.Error as e:
             print(f"エラーが発生しています:{e}")
             abort(500)
