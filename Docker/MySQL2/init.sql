472395ad11c2117b505869d85340a3fe521e7baf
ryu2026 <ryusuke.sawa.1107@gmail.com>
2026-05-19 17:59:41 +0900

    Update init.sql

--- Docker/MySQL2/init.sql
+++ Docker/MySQL2/init.sql
@@ -20,7 +20,7 @@ CREATE TABLE
     users (
         id BINARY(16) NOT NULL PRIMARY KEY,
         user_name VARCHAR(255) NOT NULL,
-        email_address VARCHAR(255) NOT NULL,
+        email_address VARCHAR(255) NOT NULL UNIQUE,
         password VARCHAR(255) NOT NULL,
         created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
         updated_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
